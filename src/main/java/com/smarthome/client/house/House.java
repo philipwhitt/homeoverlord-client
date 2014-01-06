c119350aa9c40bf9e9c94c9591943ec2dab84276
Philip Whitt <philip.whitt@bancvue.com>
2014-01-05 22:52:01 -0600

    Adding tests

--- src/main/java/com/smarthome/client/house/House.java
+++ src/main/java/com/smarthome/client/house/House.java
@@ -3,10 +3,12 @@ package com.smarthome.client.house;
 import javax.xml.bind.annotation.XmlRootElement;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.experimental.Builder;
 
 @Data
 @XmlRootElement
 @JsonIgnoreProperties(ignoreUnknown=true)
+@Builder
 public class House {
 
     private String id;
--- src/test/groovy/com/smarthome/client/house/HouseTest.groovy
+++ src/test/groovy/com/smarthome/client/house/HouseTest.groovy
@@ -5,14 +5,21 @@ import org.junit.Test;
 
 class HouseTest {
 
+    private House house;
+
     @Before
     public void setup() {
-
+        house = House.builder()
+            .id("1")
+            .isAlarmActivated(true)
+            .apiKey("c24a03360f29a0df22cd4c18cb184f0f")
+            .name("Test")
+            .build();
     }
 
     @Test
-    def void "Get should return Bean"() {
-
+    def void "Get should return house"() {
+        assert true;
     }
 
 }
